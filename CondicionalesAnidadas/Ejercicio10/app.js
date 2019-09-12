e8b2f4fe9854059ec3cc58cf5e7669dff20f7856
Alejandra <isabell_alejandra@hotmail.com>
2019-09-11 21:40:58 -0500

    Se finalizo el ejercicio 10 de Condicionales Anidadas

---
+++ CondicionalesAnidadas/Ejercicio10/app.js
@@ -0,0 +1,34 @@
+let BtnCapicua = document.getElementById('BtnCapicua');
+
+EventListener();
+
+function EventListener() {
+    BtnCapicua.addEventListener('click', VerificarCapicua);
+}
+
+function VerificarCapicua() {
+
+    let Numero = Number(document.getElementById('Numero').value);
+    let Imprimir = document.getElementById('resultado');
+    let Unidad = 0;
+    let Decena = 0;
+    let Centena = 0;
+    let UMillar = 0;
+
+
+    if (Numero < 9999 && Numero > 0) {
+        Unidad = Math.floor(Numero % 10);
+        Decena = Math.floor((Numero % 100) / 10);
+        Centena = Math.floor((Numero % 1000) / 100);
+        UMillar = Math.floor((Numero % 10000) / 1000);
+
+        if (UMillar == Unidad && Decena == Centena) {
+            Imprimir.innerText = "El número es capicua";
+        } else {
+            Imprimir.innerText = "El número no es capicua";
+        }
+
+    } else {
+        Imprimir.innerText = "El número no es capicua";
+    }
+}
