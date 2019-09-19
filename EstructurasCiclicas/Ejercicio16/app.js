73a8b0381d2644fe84b24fca2c0946a86dd1bbb9
Alejandra <isabell_alejandra@hotmail.com>
2019-09-18 22:29:52 -0500

    Se finalizo el ejercicio 16 de Estructuras Cíclicas

---
+++ EstructurasCiclicas/Ejercicio16/app.js
@@ -0,0 +1,30 @@
+let contador = 0;
+
+
+CalcularAhorro();
+
+function CalcularAhorro() {
+
+    let imprimir = document.getElementById('resultado');
+    let ahorro = 1;
+    let ahorroTotal = 0;
+
+    for (let i = 0; i <= 10; i++) {
+
+        contador++;
+        ahorro = ahorro * 3;
+        ahorroTotal = ahorroTotal + ahorro;
+
+        let newli = document.createElement('li');
+        newli.setAttribute("id", `Dia${contador}`);
+        newli.setAttribute("class", "list-group-item");
+        let newContent = document.createTextNode(`Día ${contador}: ${ahorro}`);
+        newli.appendChild(newContent);
+        let ListaDesordenada = document.getElementById('Ahorros');
+        ListaDesordenada.appendChild(newli);
+
+    }
+
+    imprimir.innerText = ahorroTotal
+
+}
