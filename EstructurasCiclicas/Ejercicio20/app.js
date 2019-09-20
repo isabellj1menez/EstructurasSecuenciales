0a52aa94a5b93628a068def23abab5607c0c6ad5
Alejandra <isabell_alejandra@hotmail.com>
2019-09-20 08:36:49 -0500

    Se modifico el app.js del ejercicio 20 de Estructuras Cíclicas

--- EstructurasCiclicas/Ejercicio20/app.js
+++ EstructurasCiclicas/Ejercicio20/app.js
@@ -10,41 +10,30 @@ EventListener();
 
 function EventListener() {
     BtnAgregarNumero.addEventListener('click', AgregarNumero);
-    valor.addEventListener('blur', validardatos);
-
 }
-
-function validardatos(Event) {
-
-    if (isNaN(Event.target.value)) {
-        Event.target.focus();
-    }
-}
-
 
 function AgregarNumero() {
 
-    do {
-        if (Number(valor.value) > 0) {
-            contadorPositivo++;
-            contador++;
-            SumaPositivos += Number(valor.value);
-            SumaTotal += Number(valor.value);
-            console.log(SumaPositivos);
-            console.log(SumaTotal)
-        } else {
+    if (!isNaN(valor.value)) {
+        do {
+            if (Number(valor.value) > 0) {
+                contadorPositivo++;
+                contador++;
+                SumaPositivos += Number(valor.value);
+                SumaTotal += Number(valor.value);
+                console.log(Number(valor.value));
+                console.log(SumaTotal)
+            } else {
 
-            contador++
-            SumaTotal += Number(valor.value);
-            console.log(SumaTotal)
+                contador++
+                SumaTotal += Number(valor.value);
+                console.log(SumaTotal)
 
-        }
+            }
+            Resultado.innerHTML = ` la cantidad de números mayores que 0 son:  ${contadorPositivo} 
+        <br> El promedio de los números positivos es:  ${SumaPositivos/contadorPositivo}
+        <br> El promedio de todos los números es: ${SumaTotal/contador}`
+        } while (valor.value == isNaN)
+
     }
-    while (valor.value == isNaN)
-    Resultado.innerHTML = ` la cantidad de números mayores que 0 son:  ${contadorPositivo} 
- <br> El promedio de los números positivos es:  ${SumaPositivos/contadorPositivo}
- <br> El promedio de todos los números es: ${SumaTotal/contador}`
-
-
-
 }
